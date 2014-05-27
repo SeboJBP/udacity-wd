1fcc8b9923d09e3cd01b7b7e8a2af2e18bfce2e4
Sebastian <sebastian.jbp@gmail.com>
2014-05-26 23:39:55 -0700

    Finished unit 2a: templates

--- unit2a-templates/templates.py
+++ unit2a-templates/templates.py
@@ -6,67 +6,38 @@ import jinja2
 import webapp2
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
 
-hidden_html = """
-<input type="hidden" name="food" value="%s">
-"""
-
-item_html = "<li>%s</li>"
-
-shopping_list_html = """
-<br>
-<br>
-<h2>Shopping List</h2>
-<ul>
-%s
-</ul>
-"""
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
     def render_str(self, template, **params):
+        "Loads file, creates jinja template and renders"
         t = jinja_env.get_template(template)
         return t.render(params)
 
     def render(self, template, **kw):
+        "Wraps around the rendered template and sends it to the client"
         self.write(self.render_str(template, **kw))
+
 
 class MainPage(Handler):
     def get(self):
-        n=self.request.get("n")
-        if n:
-            n = int(n)
-        self.render("shopping_list.html", n=n)
+        items = self.request.get_all("food")
+        self.render("shopping_list.html", items = items)
 
-
-        # output = form_html
-        # output_hidden = ""
-        
-        # items = self.request.get_all("food")
-        # if items:
-        #     output_items = ""
-        #     for item in items:
-        #         output_hidden += hidden_html % item
-        #         output_items += item_html % item
-
-        #     output_shopping = shopping_list_html % output_items
-        #     output += output_shopping
-
-        # output = output % output_hidden
-
-        # self.write(output)
 
 class FizzBuzzHandler(Handler):
     def get(self):
-        n = self.request.get("n")
+        n = self.request.get('n', 0)  # defaulting to 0
         n = n and int(n)  # this is nice syntactic sugar
         self.render('fizzbuzz.html', n = n)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/fizzbuzz', FizzBuzzHandler)], 
     debug=True)
-
